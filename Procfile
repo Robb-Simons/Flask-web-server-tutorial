bc5bc9280b5298d3f9db73b15e1ed08118c918cb
Robb-Simons <46572797+Robb-Simons@users.noreply.github.com>
2019-05-01 10:17:05 -0400

    Re-Committed

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: waitress-serve --host=0.0.0.0 helloheroku:app
+waitress-serve --call 'flaskr:create_app'
--- env/Scripts/Activate.ps1
+++ env/Scripts/Activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="C:\Users\RSimo\Documents\GitHub\heroku-projects\env"
+$env:VIRTUAL_ENV="C:\Users\RSimo\Documents\GitHub\herokuprojects\env"
 
 if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     # Set the prompt to include the env name
