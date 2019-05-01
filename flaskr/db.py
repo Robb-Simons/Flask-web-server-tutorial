04ed83592cfd2a05840e37dbf024341c5ba11d8e
Robb-Simons <46572797+Robb-Simons@users.noreply.github.com>
2019-05-01 13:42:29 -0400

    final test commit local

--- flaskr/db.py
+++ flaskr/db.py
@@ -1,4 +1,5 @@
 import sqlite3
+
 import click
 from flask import current_app, g
 from flask.cli import with_appcontext
@@ -24,6 +25,7 @@ def init_db():
 
     with current_app.open_resource('schema.sql') as f:
         db.executescript(f.read().decode('utf8'))
+
 
 @click.command('init-db')
 @with_appcontext
